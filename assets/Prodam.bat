27b45e29b6ea0a04c33b1ba3ee4a3e642055fb3e
rodcordeiro <33455212+rodcordeiro@users.noreply.github.com>
2018-12-12 14:47:03 -0200

    Add files via upload

---
+++ assets/Prodam.bat
@@ -0,0 +1,107 @@
+:: ==================================================================================
+:: NOME       : Testes de ping PRODAM
+:: AUTOR      : Rodrigo Cordeiro
+:: VERSION    : 2.0
+:: ==================================================================================
+
+
+:: Configurações de tela.
+:: /************************************************************************************/
+:mode
+
+echo off
+title Testes PRODAM
+color B
+: : ====================================================================
+:Menu
+call :print Menu
+
+echo. Teste de ping dos IP's da PRODAM.
+echo.
+echo.  Os IP's testados sao:
+echo.		-10.10.64.36;
+echo.		-10.10.64.22;
+echo.		-10.20.5.10.
+echo.
+echo.
+echo.	O que devo fazer a seguir?
+echo.
+echo.		1. Efetuar o teste de ping
+echo.		2. Pingar 10.10.64.36
+echo.		3. Pingar 10.10.64.22
+echo.		4. Pingar 10.20.5.10
+echo.		5. Reiniciar a estacao de trabalho
+echo.		6. Sair
+echo.
+echo.
+set /p option=Escolha uma opcao: 
+
+if %option% EQU 1 (
+    call :Pings
+) else if %option% EQU 2 (
+    call :ping1
+) else if %option% EQU 3 (
+    call :ping2
+) else if %option% EQU 4 (
+    call :ping3
+) else if %option% EQU 5 (
+    call :shut
+) else if %option% EQU 6 (
+    exit
+) else (
+    echo.
+    echo.Opcao invalida.
+    echo.
+    echo.Aperte alguma tecla para continuar . . .
+    pause>nul
+)
+goto Menu
+:: Escrever no alto da tela.
+:: /*************************************************************************************/
+:print
+cls
+echo.
+echo.Testes PRODAM
+echo.
+echo.%*
+echo.
+goto :eof
+:: /************************************************************************************/
+
+:Pings
+call :print Pings
+ping 10.10.64.36
+ping 10.10.64.22
+ping 10.20.5.10
+pause>nul
+goto escolha
+::==============================================================
+:ping1
+call :print Ping 10.10.64.36
+ping 10.10.64.36
+pause>nul
+goto escolha
+::===============================================================
+:ping2
+call :print Ping 10.10.64.22
+ping 10.10.64.22
+pause>nul
+goto escolha
+::===============================================================
+:ping3
+call :print Ping 10.20.5.10
+ping 10.20.5.10
+pause>nul
+goto escolha
+::===============================================================
+:shut
+call :print Reiniciar 
+echo. A estacao sera reiniciada. Deseja prosseguir?
+set /p option=A estacao sera reiniciada. Deseja prosseguir? (Sim[Y]/Nao[N])
+
+if %option% EQU Y (
+	shutdown -r -t 0
+) else (
+	goto escolha
+)
+
---
+++ js/script.js
@@ -0,0 +1,115 @@
+// Most part created by Rodrigo Cordeiro
+var uname=prompt("Hey, qual seu nome?");
+
+function alerta(){
+  if (uname=="Rodrigo"){
+    alert("Funfo!") 
+  } else {
+    alert("Hey " + uname + ','+"obrigado por visitar meu blog!!")
+
+  }
+}
+
+// Created by Tiyam Foyraz
+
+//Open The navigation bar onclicking the hamburger button
+
+function OpenNav(){
+         document.getElementById("sidenav").style.width = "210px"; 
+         document.getElementById("main").style.marginLeft = "210px"; 
+         document.getElementById("sidenav").style.backgroundColor = "#404040"; 
+         document.getElementById("b").classList.add("hide");
+         document.getElementById("a").classList.remove("hide");
+        }
+
+
+
+//Close the navigation button on clicking the cross button.
+
+function closeNav(){
+      document.getElementById("sidenav").style.width = "0"; 
+      document.getElementById("main").style.marginLeft = "0";  
+      document.getElementById("b").classList.remove("hide");
+      document.getElementById("a").classList.add("hide");
+    }
+    
+// Created by Rod Cordeiro
+function efeito(){
+  document.getElementById("xXx").style.animation= "brisa 10s linear infinite";
+  document.getElementById("xXx").style.color = "#" +(Math.random()*0xFFFFFF<<0).toString(16);
+}
+
+window.onload=function(){
+    $(".desc").slideUp(0);
+    $(".card").click(function(){
+        var top = this.offsetTop;
+        $("body").animate({
+            scrollTop : (top - 75)
+        }, 250);
+        $(".desc").slideUp(250);
+        $(".card").css({
+            "boxShadow" : "none"
+        });
+        $(".cred").css({
+            "background" : "#f2f2f2"
+        });
+        $(this).children().eq(0).css({
+            "background" : "#FFF"
+        });
+        $(this).css({
+            "boxShadow" : "0 2px 4px #111"
+        });
+        $(this).children().last().slideDown(250);
+        
+    });
+};
+
+function copia(){
+    document.getElementById("x").classList.add("copia")
+}
+function cortar(){
+    document.getElementById("xx").classList.add("cortar")
+}
+function cola(){
+    document.getElementById("xxx").classList.add("cola")
+}
+function home(){
+  document.getElementById("projeto").classList.add("none");  
+  document.getElementById("home").classList.remove("none");
+  document.getElementById("blog").classList.add("none");
+  document.getElementById("yo").classList.add("none");
+  document.getElementById('1').style.color="#fff";
+  document.getElementById('2').style.color="#aaa";
+  document.getElementById('3').style.color="#aaa";
+  document.getElementById('4').style.color="#aaa";
+}
+function projetos(){
+  document.getElementById("projeto").classList.remove("none");  
+  document.getElementById("home").classList.add("none");
+  document.getElementById("blog").classList.add("none");
+  document.getElementById("yo").classList.add("none");
+ document.getElementById('1').style.color="#aaa";
+  document.getElementById('2').style.color="#fff";
+  document.getElementById('3').style.color="#aaa";
+  document.getElementById('4').style.color="#aaa";
+}
+function blog(){
+  document.getElementById("projeto").classList.add("none");  
+  document.getElementById("home").classList.add("none");
+  document.getElementById("blog").classList.remove("none");
+  document.getElementById("yo").classList.add("none");
+document.getElementById('1').style.color="#aaa";
+  document.getElementById('2').style.color="#aaa";
+  document.getElementById('3').style.color="#fff";
+  document.getElementById('4').style.color="#aaa";
+}
+function yo(){
+  document.getElementById("projeto").classList.add("none");  
+  document.getElementById("home").classList.add("none");
+  document.getElementById("blog").classList.add("none");
+  document.getElementById("yo").classList.remove("none");
+  document.getElementById('1').style.color="#aaa";
+  document.getElementById('2').style.color="#aaa";
+  document.getElementById('3').style.color="#aaa";
+  document.getElementById('4').style.color="#fff";
+}
